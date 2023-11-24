d0e1ea61f574f194e0655992273b7e54efa30bf8
gabriellama <gabriellamalvares@gmail.com>
2023-11-23 21:48:35 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+})
+
+var cardapio = {};
+
+cardapio.eventos = {
+
+    init: () => {
+        console.log('iniciou')
+    }
+
+}
+
+cardapio.metodos = {
+
+    //obtem a lista de itens do cardapio
+    obterItensCardapio: () => {
+
+        var filtro = MENU ['burgers'];
+        console.log(filtro)
+        
+    },
+
+
+}
+
+cardapio.templates = {
+
+}
